5e34c219f09dbcdbfe08a95a6035f0825677ab3c
yashverma2110 <yash.silver2110@gmail.com>
2020-12-17 01:57:23 +0530

    env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 mongoose.connect(
-  `mongodb+srv://kanban:${process.env.MONGODB_PASSWORD}@kanban.3scqi.mongodb.net/kanbandb?retryWrites=true&w=majority`,
+  `mongodb+srv://kanban:kanban2110@kanban.3scqi.mongodb.net/kanbandb?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
